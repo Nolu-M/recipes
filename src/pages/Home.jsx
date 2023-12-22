ff945bf33a06a44e93626aa240513b135bd5dd47
Nolubabalo Maxazana <nolubabalomaxazana@gmail.com>
2023-12-22 23:05:16 +0200

    made changes to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            hello
+            <section>
+                <div>
+                    <h1>The CookBook</h1>
+
+                </div>
+            </section>
         </div>
     )
 }
