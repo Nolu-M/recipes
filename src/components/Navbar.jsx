66a2760a9406bf6b9a11899fcfa7548b425ec4bf
Nolubabalo Maxazana <nolubabalomaxazana@gmail.com>
2023-12-22 16:41:43 +0200

    CSS changes to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className=''>
+        <div className='p-4'>
             <div className='flex justify-between mb-4'>
                 <h1>Welcome Nolubabalo!</h1>
                 <div>
