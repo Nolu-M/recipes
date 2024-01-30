41c5dc14150d02c814129253cecc55a4533596cf
Nolubabalo Maxazana <nolubabalomaxazana@gmail..com>
2024-01-30 11:25:47 +0200

    Connected to Mongodb

---
+++ server.js
@@ -0,0 +1,17 @@
+/*import express from 'express';
+
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb+srv://nolubabalomaxazana:nolubabalomaxazana@cluster0.puhodvy.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Define MongoDB schema and models as needed
+
+//const YourModel = mongoose.model('YourModel', yourSchema);
+
+//Define routes and middleware
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})*/
